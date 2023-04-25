98f38d2b6d0d11a47fc0bbd872ab8027c8615597
AlbertoLaptop <albertramos902@gmail.com>
2023-04-25 02:03:20 +0200

    updated get image by choosing

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 
-//alert("Una inteligencia artificial intenta destruir a la humanidad, gana en este juego a vida o muerte para salvar el destino de la humanidad")
 // player choice until line 18
+
 let playerPlays = "";
 
 const buttonRock = document.getElementById("rock");
 buttonRock.addEventListener("click", () =>{
   playerPlays = "rock";
- 
   round();
   
 })
@@ -47,8 +46,8 @@ function checkWinner(playerChoice, computerChoice) {
   } else {
     winner = 'computer';
   }
-}
 
+}
 
 
 
@@ -60,30 +59,34 @@ function round() {
   if (result === 'tie') {
     logdiv.textContent = `You play ${playerPlays} vs computer plays ${computerChoice}, it's a tie!.`;
   } else if (result === 'player') {
+    
     logdiv.textContent = `You play ${playerPlays} vs computer plays ${computerChoice}, you win the round!`;
   } else {
     logdiv.textContent = `You play ${playerPlays} vs computer plays ${computerChoice}, computer wins the round!`;
   }
-  
+    
+  imgChoice(playerPlays); 
+  imgCompChoice(computerChoice);
   game();
 }
 
 let userScore = 0;
 let computerScore = 0;
 
+
+
 function game() {
   if (winner === 'player') {
     userScore++;
     scoreCount.innerHTML = `User score = ${userScore} </br>Computer score = ${computerScore}`;
-
+    
   } else if (winner === 'computer') {
     computerScore++;
     scoreCount.innerHTML = `User score = ${userScore} </br>Computer score = ${computerScore}`;
 
   }
-
-  console.log(`Score: Player ${userScore}, Computer ${computerScore}`);
-
+  
+//disable buttons when player reaches 5
   if (userScore === 5) {
     alert('Game over, you win!');
     buttonRock.disabled = true;
@@ -101,13 +104,40 @@ function game() {
 }
 
 
-
-//dom log
+//dom log text
 const logdiv = document.getElementById("log");
 
 
 
 //dom Count
 const scoreCount = document.getElementById("score");
-scoreCount.innerHTML = `User score = ${userScore} <br>Computer score = ${computerScore}`;
+scoreCount.innerHTML = `User score ${userScore} <br>Computer score ${computerScore}`;
+
+const userHand = document.getElementById("userHand");
+const compHand = document.getElementById("compHand");
+
+
+//change user img depending on choose
+function imgChoice(playerPlays){
+  if(playerPlays =="rock"){
+    userHand.src="images/rock.png";
+  }else if(playerPlays=="paper"){
+    userHand.src="images/paper.png";
+  }else if(playerPlays=="scissors"){
+    userHand.src="images/scissors.png"
+  }
+}
+
+
+//change comp image choose
+function imgCompChoice(computerChoice){
+  if(computerChoice =="rock"){
+    compHand.src="images/rock.png";
+  }else if(computerChoice=="paper"){
+    compHand.src="images/paper.png";
+  }else if(computerChoice=="scissors"){
+    compHand.src="images/scissors.png"
+  }
+}
+
 
